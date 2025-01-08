d60c9963bd72b2d543d1d311929d3592362cbebe
tatsart23 <Sartovuo@gmail.com>
2025-01-08 21:49:52 +0200

    Palikoita päivitetty pari kiintopistettä muutettu ja laudalla hover sekä drag vaihdettu clickkiin

---
+++ frontend/src/components/blockShapes.jsx
@@ -0,0 +1,62 @@
+const blockShapes = {
+    "red-block": [
+      { row: 0, col: 0 }, { row: 0, col: 1 },
+      { row: 1, col: 0 }, { row: 1, col: 1 },
+      { row: 2, col: 0 }
+    ],
+    "yellow-block": [
+      { row: 0, col: 0 }, { row: 0, col: 1 },
+      { row: 1, col: 0 }, { row: 2, col: 0 },
+      { row: 2, col: 1 }
+    ],
+    "green-block": [
+      { row: 0, col: 0 }, { row: 2, col: 1 },
+      { row: 1, col: 0 }, { row: 3, col: 1 },
+      { row: 1, col: 1 }
+    ],
+    "gray-block": [
+    { row: -1, col: 0 },
+    { row: 0, col: -1 },
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+    { row: 1, col: 0 },
+  ],
+    "cyan-block": [
+      { row: 0, col: 0 }, { row: 1, col: 0 },
+      { row: 1, col: 1 }, { row: 1, col: 2 },
+      { row: 1, col: 3 }
+    ],
+    "magenta-block": [
+      { row: -1, col: 0 }, { row: -1, col: 1 },
+      { row: 0, col: -1 }, { row: 0, col: 0 },
+      { row: 1, col: -1 }
+    ],
+    "purple-block": [
+      { row: 0, col: 0 }, { row: 1, col: 0 },
+      { row: 2, col: 0 }, { row: 3, col: 0 }
+    ],
+    "pink-block": [
+      { row: 0, col: 0 }, { row: 0, col: 2 },
+      { row: 0, col: 1 }, { row: 1, col: 1 },
+      { row: 0, col: 3 }
+    ],
+    "orange-block": [
+      { row: 0, col: 0 }, { row: 0, col: 1 },
+      { row: 1, col: 0 }, { row: 0, col: 2 }
+    ],
+    "lime-block": [
+      { row: 0, col: 0 }, { row: 0, col: 1 },
+      { row: 1, col: 0 }, { row: 1, col: 1 }
+    ],
+    "white-block": [
+      { row: 0, col: 0 }, { row: 0, col: 1 },
+      { row: 1, col: 0 }
+    ],
+    "light-blue-block": [
+      { row: 0, col: 0 }, { row: 0, col: 1 },
+      { row: 0, col: 2 }, { row: 1, col: 0 },
+      { row: 2, col: 0 }
+    ]
+  };
+  
+  export default blockShapes;
